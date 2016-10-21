d64522d3bcad81504fa223e51b97a521450cd67e
Matthew Aguirre <matt.aguirre@gmail.com>
2016-10-21 15:25:23 -0400

    Updated TestConsole
    Threads are now joined etc.
    Basically, this is how it *should* work when put into VBS3.

--- ConsoleTest/main.cpp
+++ ConsoleTest/main.cpp
@@ -9,52 +9,28 @@
 #include <iostream>
 
 zmq::socket_t *publisher;
-zmq::socket_t *listener;
 zmq::context_t *context;
-zmq::context_t *listener_context;
 sem_t sem;
 pthread_t thread;
-pthread_t listener_thread;
 bool running;
-bool thread2_dead;
-
-void *thread_func(void* arg)
-{
-	//zsock_t *pub = zsock_new_pub ("tcp://*:5552");
-	context = new zmq::context_t(1);
-	publisher = new zmq::socket_t(*context, ZMQ_PUB);
-	publisher->bind("tcp://*:5551");
-
-	//zctx_t *ctx = zctx_new();
- //   void *pub = zsocket_new(ctx, ZMQ_SUB);
-	//zsocket_bind(pub, "tcp://*:5552");
-
-	sem_wait(&sem);
-
-	//CANNOT CURRENTLY CLOSE W/O CRASH!
-	delete publisher;
-
-	//delete context;	// <-- causes crash if implemented
-
-	return NULL;
-}
 
 void *listener_thread_func(void* arg)
 {
-	listener_context = new zmq::context_t(1);
-	listener = new zmq::socket_t(*listener_context, ZMQ_REP);
-	listener->bind("tcp://*:5552");
+	zmq::context_t listener_context(1);
+	zmq::socket_t listener(listener_context, ZMQ_REP);
+	listener.bind("tcp://*:5552");
 
 	while(running) {
 		try {
 			zmq::message_t request;
 
 			//  Wait for next request from client
-			listener->recv (&request);
+			listener.recv (&request);
+			std::cout << request.data() << std::endl;
 		} catch(std::exception err) {
 			running = false;
+			std::cerr << "Err: " << err.what() << std::endl;
 		}
-        //std::cout << "Received Hello" << std::endl;
 
         ////  Send reply back to client
         //zmq::message_t reply (5);
@@ -62,9 +38,37 @@ void *listener_thread_func(void* arg)
         //socket.send (reply);
 	}
 
-	thread2_dead = true;
-	delete listener;
+	std::cout << "thread 2 complete" << std::endl;
 
+	return NULL;
+}
+
+void *thread_func(void* arg)
+{
+	//initialize the server
+	pthread_t listener_thread;
+	pthread_create(&listener_thread, NULL, listener_thread_func, NULL);
+
+	context = new zmq::context_t(1);
+	publisher = new zmq::socket_t(*context, ZMQ_PUB);
+	publisher->bind("tcp://*:5551");
+
+	sem_wait(&sem);
+
+	//kill the server...
+	running = false;
+	zmq::socket_t sock(*context, ZMQ_REQ);
+	zmq::message_t request(5);
+	memcpy (request.data (), "Hello", 5);
+	sock.connect("tcp://localhost:5552");
+	sock.send(request);
+
+	pthread_join(listener_thread, NULL);
+
+	//CANNOT CURRENTLY CLOSE W/O CRASH!
+	delete publisher;
+//	delete context;	// <-- causes crash if implemented
+	std::cout << "thread 1 complete" << std::endl;
 	return NULL;
 }
 
@@ -72,20 +76,13 @@ int main(int argc, char** argv) {
 	sem_init(&sem, 0, 0);
 	Sleep(1000);
 	running = true;
-	thread2_dead = false;
 	pthread_create(&thread, NULL, thread_func, NULL);
-	pthread_create(&listener_thread, NULL, listener_thread_func, NULL);
 	Sleep(1000);
 
 	sem_post(&sem);
 	Sleep(1000);
 	sem_destroy(&sem);
 
-	running = false;
-	delete listener_context;
-
-	while(!thread2_dead) {
-		Sleep(1000);
-	}
+	pthread_join(thread, NULL);
 	return 0;
 }
